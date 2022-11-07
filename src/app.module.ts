14ea6302249b557d54fc7fd262a8e3bc2c6a3338
MisterRuby <ruby8700@naver.com>
2022-11-07 11:16:30 +0900

    feat : User 스키마 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
+import { UserModule } from './domain/user/user.module';
 
 @Module({
   imports: [
@@ -10,6 +11,7 @@ import { ConfigModule } from '@nestjs/config';
       isGlobal: true,
     }),
     MongooseModule.forRoot(process.env.MONGO_URL),
+    UserModule,
   ],
   controllers: [AppController],
   providers: [AppService],
