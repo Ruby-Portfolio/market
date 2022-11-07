ff90b5ea63d5f66e5bd1235ef713054305d9da63
MisterRuby <ruby8700@naver.com>
2022-11-08 01:30:04 +0900

    update : 스키마 조회시 반환 값 변경
    Model 에서 Model & {_id} 로 변경

--- src/module/auth/auth.session.serializer.ts
+++ src/module/auth/auth.session.serializer.ts
@@ -1,21 +1,19 @@
 import { PassportSerializer } from '@nestjs/passport';
 import { Injectable } from '@nestjs/common';
-import { UserRepository } from '../../domain/user/user.repository';
 import { User } from '../../domain/user/user.schema';
+import { Types } from 'mongoose';
 
 @Injectable()
 export class SessionSerializer extends PassportSerializer {
-  constructor(private readonly userRepository: UserRepository) {
-    super();
-  }
-
   /**
    * 로그인 요청 성공시 사용자의 정보를 세션에 저장
    * @param user 로그인 요청에 성공한 유저 정보
    * @param done
    */
-  serializeUser(user: User, done: (err, user: User) => void): void {
-    // 유저 정보를 세션에 저장
+  serializeUser(
+    user: User & { _id: Types.ObjectId },
+    done: (err, user: User & { _id: Types.ObjectId }) => void,
+  ): void {
     done(null, user);
   }
 
@@ -25,13 +23,9 @@ export class SessionSerializer extends PassportSerializer {
    * @param done
    */
   async deserializeUser(
-    user: User,
-    done: (err, user: User) => void,
+    user: User & { _id: Types.ObjectId },
+    done: (err, user: User & { _id: Types.ObjectId }) => void,
   ): Promise<void> {
-    // 세션에 저장되어 있는 유저 정보가 올바른 정보인지 확인
-    const userInfo = await this.userRepository.findById(user.id);
-
-    // 세션에 유저 정보가 있다면 request.user 에 유저 정보를 추가
-    return done(null, userInfo);
+    done(null, user);
   }
 }
