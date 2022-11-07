a15bafed625fad39cba800f58f5a5841e08c4d39
MisterRuby <ruby8700@naver.com>
2022-11-07 11:16:42 +0900

    feat : User 스키마 추가

---
+++ src/domain/user/user.schema.ts
@@ -0,0 +1,31 @@
+import { Document, SchemaOptions } from 'mongoose';
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { IsEmail, IsNotEmpty, IsPhoneNumber, IsString } from 'class-validator';
+
+const options: SchemaOptions = {
+  timestamps: true,
+};
+
+@Schema(options)
+export class User extends Document {
+  @IsEmail()
+  @IsNotEmpty()
+  @Prop({ required: true })
+  email: string;
+
+  @IsNotEmpty()
+  @Prop({ required: true })
+  password: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @Prop({ required: true })
+  name: string;
+
+  @IsPhoneNumber()
+  @IsNotEmpty()
+  @Prop({ required: true })
+  phone: string;
+}
+
+export const CatSchema = SchemaFactory.createForClass(User);
